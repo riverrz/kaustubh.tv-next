71dc2927dd08d51c7dc6f866bcfa8f6c01486c75
Shivam Kumar <shivam123425@gmail.com>
2020-02-09 14:44:40 +0530

    Initial boilerplate

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Index = () => (
+  <Layout>
+    <div>
+      <h1>Welcome to Bitz Price</h1>
+    </div>
+  </Layout>
+);
+
+export default Index;
